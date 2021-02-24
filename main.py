fdec67af544b48d5b9d3d190a7d6a5d70be1b6a2
aahnik <66209958+aahnik@users.noreply.github.com>
2021-02-24 17:47:25 +0530

    serve html instead of text or json

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from starlette.responses import RedirectResponse, Response
+from fastapi.templating import Jinja2Templates
+
 
 app = FastAPI(title="fastapi-url-shortener")
-
+templates = Jinja2Templates(directory='templates')
 
 db = {}
+
 
 class ShortUrl(BaseModel):
     slug: str
     url: str
 
+
 @app.get('/')
-def index():
-    return Response(content='Server is up and running',status_code=200)
+def index(request: Request):
+
+    return templates.TemplateResponse('index.html',
+                                      context={'request': request},
+                                      status_code=200)
+
 
 @app.post('/shorten')
-def shorten(sh:ShortUrl):
+def shorten(sh: ShortUrl):
     db[sh.slug] = sh.url
-    return Response(content='Success',status_code=201)
+    return Response(content='Success', status_code=201)
+
 
 @app.get('/{slug}')
-def redirect(slug:str):
+def redirect(slug: str, request: Request):
+    print(slug)
     if slug in db:
         return RedirectResponse(url=db[slug])
     else:
-        return Response(status_code=404)
-
+        return templates.TemplateResponse('404.html',
+                                          context={'request': request},
+                                          status_code=404)
